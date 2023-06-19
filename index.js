2da1ef95720f4a61f77565f72da5176ef188ab35
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-18 22:17:32 -0700

    feat: create express server

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import dbConnect from './utils/dbConnect.js'
+import { routes } from './routes/routes.js'
+
+const app = express()
+dbConnect()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
