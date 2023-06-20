759411da3a6f600f45ff1c6c5e896338f495fc71
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-19 18:35:25 -0700

    refactor: change route names

--- routes/routes.js
+++ routes/routes.js
@@ -6,18 +6,15 @@ import {
   resetPassword,
   updateUser,
 } from '../controllers/user.controller.js'
-import { 
-  addHashtag, 
-  getHashtag 
-} from '../controllers/hashtag.controller.js'
+import { addHashtag, getHashtag } from '../controllers/hashtag.controller.js'
 
 export const routes = Router()
 
-routes.get('/user/:id', getUser)
-routes.get('/user/username/:username', getUsersByUsername)
-routes.post('/user', addUser)
-routes.put('/user/:id', updateUser)
-routes.put('/user/password/:id', resetPassword)
+routes.get('/users/:id', getUser)
+routes.get('/users/username/:username', getUsersByUsername)
+routes.post('/users', addUser)
+routes.put('/users/:id', updateUser)
+routes.put('/users/password/:id', resetPassword)
 
-routes.get('/hashtag/:name', getHashtag)
-routes.post('/hashtag', addHashtag)
+routes.get('/hashtags/:name', getHashtag)
+routes.post('/hashtags', addHashtag)
