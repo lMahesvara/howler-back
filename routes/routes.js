c79a4aabc68b1712e77ee2e820382f0e7cca93a5
German <german_gomez1205@hotmail.com>
2023-06-20 18:34:32 -0700

    Ft. howl routes

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,7 @@ import {
 import { addHashtag, getHashtag } from '../controllers/hashtag.controller.js'
 import { getChats, getChat, read } from '../controllers/chat.controller.js'
 import { addMessage, getMessages } from '../controllers/message.controller.js'
+import { addHowl, getHowlById, getHowlsByHashtag, getHowlsByUserId } from '../controllers/howl.controller.js'
 
 export const routes = Router()
 
@@ -27,3 +28,8 @@ routes.put('/chats/read/:idChat/:idUser', read)
 
 routes.get('/messages/:idChat', getMessages)
 routes.post('/messages/:idChat', addMessage)
+
+routes.get('/howls/:idHowl',getHowlById)
+routes.get('/howls/user/:idUser',getHowlsByUserId)
+routes.get('/howls/hashtag/:hashtag', getHowlsByHashtag)
+routes.post('/howls', addHowl)
