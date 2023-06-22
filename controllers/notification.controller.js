df3679e40c5be464892ba440e01599b5ef0a45f6
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-21 17:11:38 -0700

    fix: change readNotification behavior

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -9,6 +9,14 @@ export const readNotification = async (req, res) => {
 
   try {
     const notification = await Notification.findById(id)
+
+    if (!notification) {
+      return res.status(404).json({ message: 'Notification not found' })
+    }
+
+    notification.read = true
+    await notification.save()
+
     res.json(notification)
   } catch (error) {
     res.status(500).json({ message: error.message })
@@ -33,18 +41,23 @@ export const getNotifications = async (req, res) => {
 }
 
 export const addNotification = async (req, res) => {
- 
-  const { userTo,userFrom, type, read, howl, title, body } = req.body
+  const { userTo, userFrom, type, howl, title, body } = req.body
 
-  if (!title || !userTo || !type || !userFrom || !howl) {
+  if (!userTo || !type || !userFrom) {
     return res.status(400).json({ message: 'Please provide all fields' })
   }
   try {
-    const notification = new Notification({ userTo, userFrom, type, read, howl, title,body })
+    const notification = new Notification({
+      userTo,
+      userFrom,
+      type,
+      howl,
+      title,
+      body,
+    })
     await notification.save()
-    res.json(notification)
+    res.status(201).json(notification)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
-
